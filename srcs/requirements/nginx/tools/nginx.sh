0b29eea586dc92b04d4f740c095ec2da5c46e3b8
Nekane <125030234+nmontiel42@users.noreply.github.com>
2024-08-06 16:53:23 +0200

    Add files via upload

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-mkdir /etc/nginx/ssl
+mkdir -p /etc/nginx/ssl
 
-#Para crear el certificado ssl y configurar wordpress automaticamente
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out $CERTIFICATE -keyout $CERT_KEY -subj "/C=SP/ST=Andalucia/L=Malaga/O=42/OU=42/CN=$DOMINIO_NAME/UID=$LOGIN"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out "$CERTIFICATE" -keyout "$CERT_KEY" -subj "/C=ES/ST=Andalucia/L=Malaga/O=42/OU=42/CN=$DOMINIO_NAME/UID=$LOGIN"
 
-#sed -i "s/dominio_name/$DOMINIO_NAME/" /etc/nginx/nginx.conf
-#sed -i "s#certificate#$CERTIFICATE#g" /etc/nginx/nginx.conf
-#sed -i "s#cert_key#$CERT_KEY#g" /etc/nginx/nginx.conf
+sed -i "s/dominio_name/$DOMINIO_NAME/" /etc/nginx/nginx.conf
+sed -i "s#ssl_certificate /etc/nginx/ssl/nginx-selfsigned.crt;#ssl_certificate $CERTIFICATE;#" /etc/nginx/nginx.conf
+sed -i "s#ssl_certificate_key /etc/nginx/ssl/nginx-selfsigned.key;#ssl_certificate_key $CERT_KEY;#" /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+mkdir -p /run/php
+
+chown -R www-data.www-data /var/www/html/wordpress
+
+chmod -R 755 /var/www/html/wordpress
+
+sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = wordpress:9000#g' /etc/php/7.4/fpm/pool.d/www.conf
+
+mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+
+sed -i "s/database_name_here/$MARIA_DATABASE/" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$MARIA_USER/" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$MARIA_PASS/" /var/www/html/wordpress/wp-config.php
+sed -i "s/put your unique phrase here/$PHRASE/" /var/www/html/wordpress/wp-config.php
+sed -i "s/localhost/mariadb:3306/" /var/www/html/wordpress/wp-config.php
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core install --allow-root --url=$DOMINIO_NAME --title=NekaWeb --admin_user=$USER_WP --admin_password=$ADMIN_PASS_WP --admin_email=$ADMIN_EMAIL_WP --skip-email --path=/var/www/html/wordpress
+
+wp user create --allow-root $NEW_USER_WP $WPUSER_EMAIL --user_pass=$WPUSER_PASS --path=/var/www/html/wordpress --url=$DOMINIO_NAME
+
+/usr/sbin/php-fpm7.4 -F
